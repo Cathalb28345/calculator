cd1b5c08c95c9bc210646837f570e3e9269dc03c
Cathalb28345 <cathalb4@live.com>
2017-03-21 10:27:53 +0000

    Update CalcLogic.java

--- CalcLogic.java
+++ CalcLogic.java
@@ -1,59 +1,67 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class CalcLogic
-{
-//-- Instance variables.
-private int _currentTotal;   // The current total is all we need to remember.
-
-/** Constructor */
-public CalcLogic()
 	{
-		_currentTotal = 0;
+		//-- Instance variables.
+		// The current total is all we need to remember.
+		private double StoredVal = 0.0; 
+		/** Constructor */
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("JavaScript");
+	    
+	public String Evaluate(String sum)
+		{
+			try 
+				{
+					return engine.eval(sum) + "" ;
+				} 
+			catch (ScriptException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();		
+				}
+			return "";
+		}
+	
+	//-----------------CONVERT ALL THE STRINGS ENTER ON THE PAD TO NUMERIC DOUBLES-------------------------------------------*/
+		private double convertToNumber(String n)
+			{
+				return Double.parseDouble(n);
+			}
+		public void Square(Double n) 
+		{	
+				//Double.parseDouble(n);
+				Double square = Math.pow(n * n, n);
+				Double.toString( n );
+		}
+		
+		
+	//----------------------------------MEMORY FUNCTIONS FOR THE CALCULATOR STARTS --------------------------------------------------------///////////////////////////////////////////////////
+		public void Store(String displayText) 
+			{
+				StoredVal = Double.parseDouble(displayText);
+			}
+		
+		public double GetStoredVaL()
+			{
+				return StoredVal;
+			}
+		
+		public void ResetStoredVal()
+			{
+				StoredVal = 0.0;
+			}
+		
+		public void PlusStoredVal(double n) 
+			{
+					StoredVal += n;
+			}
+		
+		public void MinusStoredVal(double n) 
+			{
+					StoredVal -= n;
+			}
+///-----------------------------MEMORY FUNTIONS FOR THE CALCULATOR ENDS-------------------------------------------------------------/////
 	}
-
-public String getTotalString()
-	{
-		return "" + _currentTotal;
-	}
-
-public void setTotal(String n) 
-	{
-		_currentTotal = convertToNumber(n);
-	}
-
-public void add(String n)
-	{
-		_currentTotal += convertToNumber(n);
-	}
-
-public void subtract(String n) 
-	{
-		_currentTotal -= convertToNumber(n);
-	}
-
-public void multiply(String n)
-	{
-		_currentTotal *= convertToNumber(n);
-	}
-
-public void divide(String n) 
-	{
-		_currentTotal /= convertToNumber(n);
-	}
-////////////////////////////////////////////////////////////////////////////////////////
-public void square(String n) 
-{
-	_currentTotal = convertToNumber(n);
-}
-public void squareRoot(String n) 
-{
-	_currentTotal = convertToNumber(n);
-}
-public void Percent(String n) 
-{
-	_currentTotal = convertToNumber(n);
-}
-////////////////////////////////////////////////////////////////////////////////////////
-private int convertToNumber(String n)
-	{
-		return Integer.parseInt(n);
-	}
-}
